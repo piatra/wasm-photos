bd09f3ef2aa7c0f69e97c5e74f434f7655bbac97
Andrei Oprea <andrei.br92@gmail.com>
2018-04-22 23:45:36 +0200

    read all files in folder

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,17 @@ use std::fmt::Write;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
+use exif::{Value, Tag, DateTime};
 
 fn main() {
+    let path = Path::new("./photos");
+    for entry in path.read_dir().expect("read_dir call failed") {
+        if let Ok(entry) = entry {
+            if let Err(e) = dump_file(&entry.path()) {
+                println!("{}: {}", path.display(), e);
+            }
+        }
+    }
     for path in env::args_os().skip(1).map(PathBuf::from) {
         if let Err(e) = dump_file(&path) {
             println!("{}: {}", path.display(), e);
@@ -19,14 +28,34 @@ fn dump_file(path: &Path) -> Result<(), exif::Error> {
     let reader = try!(exif::Reader::new(&mut BufReader::new(&file)));
 
     println!("{}", path.display());
-    for f in reader.fields() {
-        let thumb = if f.thumbnail { "1/" } else { "0/" };
-        println!("  {}{}: {}", thumb, f.tag, f.value.display_as(f.tag));
-        if let exif::Value::Ascii(ref s) = f.value {
-            println!("      Ascii({:?})",
-                     s.iter().map(escape).collect::<Vec<_>>());
-        } else {
-            println!("      {:?}", f.value);
+    // for f in reader.fields() {
+    //     let thumb = if f.thumbnail { "1/" } else { "0/" };
+    //     println!("  {}{}: {}", thumb, f.tag, f.value.display_as(f.tag));
+    //     if let exif::Value::Ascii(ref s) = f.value {
+    //         println!("      Ascii({:?})",
+    //                  s.iter().map(escape).collect::<Vec<_>>());
+    //     } else {
+    //         println!("      {:?}", f.value);
+    //     }
+    // }
+    if let Some(field) = reader.get_field(Tag::DateTime, false) {
+        match field.value {
+            Value::Ascii(ref vec) if !vec.is_empty() => {
+                if let Ok(datetime) = DateTime::from_ascii(vec[0]) {
+                    println!("Year of DateTime is {}.", datetime.year);
+                }
+            },
+            _ => {},
+        }
+    }
+    if let Some(field) = reader.get_field(Tag::PixelXDimension, false) {
+        if let Some(width) = field.value.get_uint(0) {
+            println!("Valid width of the image is {}.", width);
+        }
+    }
+    if let Some(field) = reader.get_field(Tag::PixelYDimension, false) {
+        if let Some(height) = field.value.get_uint(0) {
+            println!("Valid height of the image is {}.", height);
         }
     }
     Ok(())
